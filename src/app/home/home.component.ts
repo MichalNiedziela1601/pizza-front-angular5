f60e16c77e36542bcdc654e6cf9dbea20af9d11b
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2018-01-12 20:42:18 +0000

    add offer section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,5 +7,10 @@ import {Component} from "@angular/core";
 })
 
 export default class HomeComponent {
+  boxOffers = [
+    { src: 'assets/img/home/bg_pizza_oferta.png', title: 'pizza'},
+    { src: 'assets/img/home/bg_daniagorace_oferta.png', title: 'hot dishes'},
+    { src: 'assets/img/home/bg_salatki_oferta.png', title: 'salads'},
 
+  ]
 }
